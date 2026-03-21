f9000cccec49db1ee34a66c98bbdb15308059af4
daisyroid <daisy@yuimax.org>
2026-03-21 13:26:56 +0900

    cmt: 2026-03-21 13:26 (JST)

--- www/lib/memo-script.js
+++ www/lib/memo-script.js
@@ -5,11 +5,11 @@ function onClick(ev) {
     .then(() => {
       // メッセージボックスを作る
       const msgbox = document.createElement('div');
-      msgbox.className = 'toast-msgbox';
+      msgbox.className = 'toast-message';
       msgbox.textContent = 'コピーしました！';
-      msgbox.style.left = `${ev.pageX - 120}px`;
+      msgbox.style.left = `${ev.pageX - 100}px`;
       msgbox.style.top = `${ev.pageY - 60}px`;
-      // 表示して、0.8秒後に透明化開始、その0.2秒後に消滅
+      // 表示し、0.8秒後に透明化、その0.2秒後に消滅
       document.body.appendChild(msgbox);
       setTimeout(() => {
         msgbox.style.opacity = '0';
@@ -29,7 +29,7 @@ function onSubmit(ev) {
       return;
     }
   }
-  // submitする場合はボタンを無効化（2度押し防止)
+  // submitする前にボタンを無効化（2度押し防止)
   for (const btn of document.querySelectorAll("button")) {
     btn.disabled = true;
   }
